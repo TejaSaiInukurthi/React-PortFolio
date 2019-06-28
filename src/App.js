060d52cbe990bae7d474fb687297a3b7fd2e067f
TejaSaiInukurthi <tejasaichowdary02@gmail.com>
2019-06-28 19:52:41 +0530

    changing router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
-import { Link } from 'react-router-dom';
+import { Link,HashRouter} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <HashRouter basename='/'>
       <div className="demo-big-content">
     <Layout>
         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
@@ -31,6 +32,7 @@ class App extends Component {
         </Content>
     </Layout>
 </div>
+</HashRouter>
 
     );
   }
